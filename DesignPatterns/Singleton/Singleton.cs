7447ebe31de339cf8e308757e26abd535acfa9fa
Maz Hussain <mmqhussain2014@hotmail.co.uk>
2016-03-14 22:26:06 +0000

    Committing Singleton Design Pattern to Design Patterns Project

---
+++ DesignPatterns/Singleton/Singleton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    // thread safe implementation of the singleton class.
+    public class Singleton
+    {
+        private static Singleton _instance = null;
+        private static readonly object _padlock = new object();
+
+        public static Singleton Instance
+        {
+            get
+            {
+                lock (_padlock)
+                {
+                    if(_instance == null)
+                    {
+                        _instance = new Singleton();
+                    }
+                    return _instance;
+                }
+                
+            }
+        }
+    }
+}
