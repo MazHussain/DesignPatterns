be867456aeb31a8f4826d35e75348d94bd25d3f4
Maz Hussain <mmqhussain2014@hotmail.co.uk>
2016-03-14 22:56:38 +0000

    added private constructor to singleton to prevent it from being instantiated.

--- DesignPatterns/Singleton/Singleton.cs
+++ DesignPatterns/Singleton/Singleton.cs
@@ -6,10 +6,15 @@
         private static Singleton _instance = null;
         private static readonly object _padlock = new object();
 
+        // prevents class from being instantiated.
+        private Singleton()
+        {}
+
         public static Singleton Instance
         {
             get
             {
+                //ensures thread safety
                 lock (_padlock)
                 {
                     if(_instance == null)
